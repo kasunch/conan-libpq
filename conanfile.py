6bf596d40d61f3ecfae5217f49f621d3724aba53
Uilian Ries <uilianries@gmail.com>
2018-06-17 23:37:34 -0300

    Build package on Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,83 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-
-    # Indicates License type of the packaged library
-    license = "MIT"
-
-    # Packages the license for the conanfile.py
+class LibpqConan(ConanFile):
+    name = "libpq"
+    version = "10.4"
+    description = "The library used by all the standard PostgreSQL tools."
+    url = "https://github.com/bincrafters/conan-libpq"
+    homepage = "https://www.postgresql.org/docs/current/static/libpq.html"
+    license = "PostgreSQL"
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
-
-    # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
+    build_subfolder = None
+    autotools = None
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
-    def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+    def configure(self):
+        del self.settings.compiler.libcxx
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
+    def source(self):
+        source_url = "https://ftp.postgresql.org/pub/source"
+        tools.get("{0}/v{1}/postgresql-{2}.tar.gz".format(source_url, self.version, self.version))
+        extracted_dir = "postgresql-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        cmake.configure(build_folder=self.build_subfolder)
-        return cmake
+    def configure_autotools(self):
+        if not self.autotools:
+            self.autotools = AutoToolsBuildEnvironment(self)
+            self.build_subfolder = os.path.join(self.build_folder, "output")
+            args = ['--without-readline']
+            args.append('--prefix={}'.format(self.build_subfolder))
+            with tools.chdir(self.source_subfolder):
+                self.autotools.configure(args=args)
+        return self.autotools
 
     def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        autotools = self.configure_autotools()
+        with tools.chdir(os.path.join(self.source_subfolder, "src", "interfaces", "libpq")):
+            autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="COPYRIGHT", dst="licenses", src=self.source_subfolder)
+        autotools = self.configure_autotools()
+        with tools.chdir(os.path.join(self.source_subfolder, "src", "interfaces", "libpq")):
+            autotools.install()
+        self.copy(pattern="*.h", dst="include", src=os.path.join(self.build_subfolder, "include"))
+        self.copy(pattern="postgres_ext.h", dst="include", src=os.path.join(self.source_subfolder, "src", "include"))
+        self.copy(pattern="pg_config_ext.h", dst="include", src=os.path.join(self.source_subfolder, "src", "include"))
+        if self.settings.os == "Linux":
+            pattern = "*.so*" if self.options.shared else "*.a"
+        elif self.settings.os == "Macos":
+            pattern = "*.dylib" if self.options.shared else "*.a"
+        elif self.settings.os == "Windows":
+            pattern = "*.dll" if self.options.shared else "*.lib"
+        self.copy(pattern=pattern, dst="lib", src=os.path.join(self.build_subfolder, "lib"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
