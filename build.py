bff9ea9f51ceb12beec3eb6c0680393f615c1815
Uilian Ries <uilianries@gmail.com>
2018-07-19 00:24:39 -0300

    Build on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -5,7 +5,5 @@ from conans import tools
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    shared_option_name = False if tools.os_info.is_windows else None
-    builder = build_template_default.get_builder(shared_option_name=shared_option_name)
-
+    builder = build_template_default.get_builder()
     builder.run()
