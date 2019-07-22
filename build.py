4094e0183310de5b4187b44d5de067221914b831
Kasun Hewage <kasun.ch@gmail.com>
2019-07-22 19:11:42 +0200

    Changes

--- build.py
+++ build.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from conans import tools
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     shared_option_name = False if tools.os_info.is_windows else None
-    builder = build_template_default.get_builder(shared_option_name=shared_option_name)
-
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name=shared_option_name)
     builder.run()
